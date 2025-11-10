06d9e94385fcbd4e0063f0d532b5dbafdab75867
이경석2171073 <115882949+Bigben2002@users.noreply.github.com>
2025-11-10 19:07:48 +0900

    Delete KioskApp-main (1)/KioskApp-main directory

--- KioskApp-main (1)/KioskApp-main/Kiosk/app/src/main/java/com/example/kiosk/data/model/CartItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kiosk.data.model
-
-data class CartItem(
-    val menuItem: MenuItem,
-    var quantity: Int
-)
--- KioskApp-main (1)/KioskApp-main/Kiosk/app/src/main/java/com/example/kiosk/data/model/ItemOption.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kiosk.data.model
-
-data class ItemOption(
-    val name: String,
-    val price: Int = 0
-)
